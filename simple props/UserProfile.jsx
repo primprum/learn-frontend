999cbf55224b9b01b237a656f8f30e14ca1fcdc9
Prima Wirawan <primailkom@gmail.com>
2023-10-05 14:07:14 +0700

    Create simple props

---
+++ simple props/UserProfile.jsx
@@ -0,0 +1,12 @@
+import UserInfo from "./UserInfo";
+
+function UserProfile() {
+  return (
+    <div>
+      <h1>User Profile</h1>
+      <UserInfo name="Prima Wirawan" email="primailkom@gmail.com" />
+    </div>
+  );
+}
+
+export default UserProfile;
